290a75a99ea24796f9fdb16343a4854f294d2696
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-11-20 17:39:06 -0700

    Modify various files and add photo
    Added a photo of coffee. Commented out parts of index and style in
    dist directory. Modified style in src. Modified webpack config
    file to load images. Added elements to home file.

--- src/home.js
+++ src/home.js
@@ -5,10 +5,14 @@
 // export { printMe };
 
 import './style.css';
+import coffee from './coffee.jpg';
 
 function home() {
     const content = document.querySelector('#content');
 
+    content.classList.add('flex-settings');
+
+    // menu bar
     const menuContainer = document.createElement('div');
 
     content.appendChild(menuContainer);
@@ -26,6 +30,40 @@ function home() {
     contact.textContent = "contact";
 
     menuContainer.classList.add('menu-container');
+
+    // title
+    const title = document.createElement('div');
+
+    content.appendChild(title);
+
+    title.textContent = "Lorem Ipsum Restaurant";
+
+    title.classList.add('title');
+
+    // description
+    const description = document.createElement('div');
+
+    content.appendChild(description);
+
+    description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt tempor libero et bibendum.Donec et nulla sit amet nisi pellentesque tincidunt.Phasellus mattis accumsan auctor.Integer vulputate metus leo, et bibendum nisi pulvinar nec.Aliquam eros magna, tristique vitae sapien ac, dapibus imperdiet lorem.";
+
+    description.classList.add('description');
+
+    // image
+    const picture = new Image();
+    picture.src = coffee;
+
+    content.appendChild(picture);
+
+    picture.classList.add('image-settings');
+
+    // credit to author of picture (Caleb on Unsplash)
+    const photoCredit = document.createElement('p');
+    content.appendChild(photoCredit);
+    photoCredit.textContent = "Photo by Caleb on Unsplash";
+
+    // https://unsplash.com/@calebrussell?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+    // https://unsplash.com/photos/white-ceramic-cup-with-saucer-on-brown-wooden-table-_hnL_961xTk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
 }
 
 export { home };
